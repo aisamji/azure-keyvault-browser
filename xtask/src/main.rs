0014764cf6c9720847879d9a50f2f2b15bf4b199
Ali Samji <aisamji@users.noreply.github.com>
2025-05-06 09:10:02 -0500

    Add version subcommand for cargo xtask.

--- xtask/src/main.rs
+++ xtask/src/main.rs
@@ -1,3 +1,25 @@
+use cargo_metadata::MetadataCommand;
+use clap::{Command, command};
+
 fn main() {
-    println!("Hello, world!");
+    let matches = command!()
+        .subcommand(
+            Command::new("version")
+                .about("Print the versions of the specified crates.")
+        )
+        .get_matches();
+
+    if let Some(_) = matches.subcommand_matches("version") {
+        let metadata = MetadataCommand::new()
+            .no_deps()
+            .exec()
+            .expect("Failed to run cargo metadata");
+
+        for p in metadata.packages {
+            // TODO: Restrict to only printing out packages in the list of packages passed in if
+            // available.
+            println!("{}: {}", p.name, p.version);
+            // TODO: Only print out version number if only a single package is provided.
+        }
+    }
 }
