c911b67b1075d2041211600c811db71bb1d5d1a7
Ali Samji <aisamji@users.noreply.github.com>
2025-06-19 07:14:09 -0500

    refactor/generic macros (#26)
    * refactor: make azure-keyvault-tui-macros more generic and flexible
    
    - Rename update_progress\! macro to notify\! for more generic usage
    - Make all macro parameters optional to reduce coupling:
      - Optional callback: return None when not specified
      - Optional event_enum: no sender parameter when not specified
      - Optional error_variant: use eprintln\! fallback when not specified
    - Change spawn_task return type to Option<JoinHandle<()>>
    - Add publish = false to prevent accidental publishing
    - Update background task manager to handle optional JoinHandle
    
    * refactor: rename derive macro and parameters to be more generic
    
    - Rename BackgroundTaskSpec derive macro to TaskSpec for broader applicability
    - Rename event_enum parameter to message_type (clearer intent for message passing)
    - Rename error_variant parameter to abort_with (clearer intent for abort macro)
    - Update all struct fields and usage throughout codebase
    - Maintain backward compatibility with existing functionality
    
    These changes prepare the macros for extraction into a generic task spawning library.
    
    * refactor: rename ListKeyVaults to LoadKeyVaults.
    
    * refactor: Rename azure-keyvault-tui-macros to async-spawn-macros.
    
    * ci: Ensure test and upload-binary workflow can share the rust-cache.
    
    * style: Cargo fmt fix.

---
+++ crates/async-spawn-macros/src/lib.rs
@@ -0,0 +1,350 @@
+use darling::{FromMeta, ast::NestedMeta};
+use proc_macro::TokenStream;
+use proc_macro_error::{abort, proc_macro_error};
+use proc_macro2::TokenStream as TokenStream2;
+use quote::quote;
+use syn::{
+    Attribute, Data, DeriveInput, Fields, FnArg, ItemFn, Meta, Variant, parse_macro_input,
+    parse_quote, parse2, spanned::Spanned,
+};
+
+#[derive(FromMeta)]
+struct CallbackArgs {
+    message_type: Option<syn::Path>,
+    abort_with: Option<syn::Expr>,
+}
+
+#[proc_macro_error]
+#[proc_macro_attribute]
+pub fn background_task(args: TokenStream, item: TokenStream) -> TokenStream {
+    let input = parse_macro_input!(item as ItemFn);
+    let attr_args = match NestedMeta::parse_meta_list(args.into()) {
+        Ok(args) => args,
+        Err(e) => abort!(e.span(), "Invalid attribute arguments: {}", e),
+    };
+    let args = match CallbackArgs::from_list(&attr_args) {
+        Ok(args) => args,
+        Err(e) => return TokenStream::from(e.write_errors()),
+    };
+    background_task_impl(input, args).into()
+}
+
+fn background_task_impl(mut function: ItemFn, args: CallbackArgs) -> TokenStream2 {
+    // Only add event sender parameter if event_enum is specified
+    if let Some(ref message_type) = args.message_type {
+        function
+            .sig
+            .inputs
+            .insert(0, event_sender_arg(message_type));
+    }
+
+    let notify_macro = if args.message_type.is_some() {
+        notify_macro()
+    } else {
+        eprintln_notify_macro()
+    };
+
+    let abort_macro = if let Some(ref abort_with) = args.abort_with {
+        abort_macro(abort_with)
+    } else {
+        eprintln_abort_macro()
+    };
+
+    let block = function.block.clone();
+    function.block = parse2(quote! {
+        {
+            #notify_macro
+            #abort_macro
+            #block
+        }
+    })
+    .unwrap_or_else(|e| {
+        abort!(
+            proc_macro2::Span::mixed_site(),
+            "Failed to parse function block: {}",
+            e
+        );
+    });
+    quote! { #function }
+}
+
+fn event_sender_arg(event_type: &syn::Path) -> FnArg {
+    let event_type_ident: syn::Type = syn::parse_quote!(#event_type);
+    parse_quote!(tx: tokio::sync::mpsc::Sender<#event_type_ident>)
+}
+
+fn notify_macro() -> TokenStream2 {
+    quote! {
+        macro_rules! notify {
+            ($variant: expr) => {
+                if let Err(_) = tx.send($variant).await {
+                    return;
+                }
+            }
+        }
+    }
+}
+
+fn eprintln_notify_macro() -> TokenStream2 {
+    quote! {
+        macro_rules! notify {
+            ($($arg:tt)*) => {
+                eprintln!($($arg)*);
+            }
+        }
+    }
+}
+
+fn abort_macro(abort_with: &syn::Expr) -> TokenStream2 {
+    quote! {
+        macro_rules! abort {
+            ($($arg:tt)*) => {
+                {
+                    let message = format!($($arg)*);
+                    let _ = tx.send(#abort_with(message.clone())).await.inspect_err(|_| eprintln!("{}", message));
+                    return;
+                }
+            }
+        }
+    }
+}
+
+fn eprintln_abort_macro() -> TokenStream2 {
+    quote! {
+        macro_rules! abort {
+            ($($arg:tt)*) => {
+                {
+                    eprintln!($($arg)*);
+                    return;
+                }
+            }
+        }
+    }
+}
+
+#[derive(FromMeta)]
+struct EnumArgs {
+    message_type: syn::Path,
+}
+
+#[derive(FromMeta)]
+struct VariantArgs {
+    callback: syn::Ident,
+}
+
+#[proc_macro_error]
+#[proc_macro_derive(TaskSpec, attributes(taskspec))]
+pub fn task_spec_derive(input: TokenStream) -> TokenStream {
+    let input = parse_macro_input!(input as DeriveInput);
+    task_spec_impl(input).into()
+}
+
+fn task_spec_impl(input: DeriveInput) -> TokenStream2 {
+    let enum_name = &input.ident;
+
+    // Parse the event_type from the derive macro arguments
+    let event_type = extract_message_type(&input.attrs);
+
+    let variants = match input.data {
+        Data::Enum(data_enum) => data_enum.variants,
+        _ => abort!(
+            input.ident.span(),
+            "TaskSpec can only be derived for enums, found {}",
+            match input.data {
+                Data::Struct(_) => "struct",
+                Data::Union(_) => "union",
+                _ => "unknown type",
+            }
+        ),
+    };
+
+    let spawn_arms = variants
+        .iter()
+        .map(|v| generate_spawn_arm(v, event_type.is_some()))
+        .collect::<Vec<_>>();
+
+    if let Some(event_type) = event_type {
+        let event_type_ident: syn::Type = syn::parse_quote!(#event_type);
+        quote! {
+            impl #enum_name {
+                /// Spawns a background task for this task specification.
+                ///
+                /// This method consumes `self` to move the contained data into the spawned task.
+                /// Returns a `JoinHandle` that can be used to await completion of the background task.
+                /// Returns `None` if no callback is specified for the variant.
+                pub fn spawn_task(self, tx: &tokio::sync::mpsc::Sender<#event_type_ident>) -> Option<tokio::task::JoinHandle<()>> {
+                    match self {
+                        #(#spawn_arms)*
+                    }
+                }
+            }
+        }
+    } else {
+        quote! {
+            impl #enum_name {
+                /// Spawns a background task for this task specification.
+                ///
+                /// This method consumes `self` to move the contained data into the spawned task.
+                /// Returns a `JoinHandle` that can be used to await completion of the background task.
+                /// Returns `None` if no callback is specified for the variant.
+                pub fn spawn_task(self) -> Option<tokio::task::JoinHandle<()>> {
+                    match self {
+                        #(#spawn_arms)*
+                    }
+                }
+            }
+        }
+    }
+}
+
+fn generate_spawn_arm(variant: &Variant, has_sender: bool) -> TokenStream2 {
+    let variant_name = &variant.ident;
+    let callback_name = extract_callback_name(&variant.attrs);
+
+    match &variant.fields {
+        Fields::Unit => {
+            if let Some(callback) = callback_name {
+                if has_sender {
+                    quote! {
+                        Self::#variant_name => {
+                            let tx_clone = tx.clone();
+                            Some(tokio::task::spawn(async move {
+                                #callback(tx_clone).await;
+                            }))
+                        }
+                    }
+                } else {
+                    quote! {
+                        Self::#variant_name => {
+                            Some(tokio::task::spawn(async move {
+                                #callback().await;
+                            }))
+                        }
+                    }
+                }
+            } else {
+                quote! {
+                    Self::#variant_name => None
+                }
+            }
+        }
+        Fields::Unnamed(fields) => {
+            let field_names: Vec<syn::Ident> = (0..fields.unnamed.len())
+                .map(|i| syn::Ident::new(&format!("field_{}", i), proc_macro2::Span::mixed_site()))
+                .collect();
+
+            let pattern = quote! { Self::#variant_name(#(#field_names),*) };
+
+            if let Some(callback) = callback_name {
+                if has_sender {
+                    let args = quote! { tx_clone, #(#field_names),* };
+                    quote! {
+                        #pattern => {
+                            let tx_clone = tx.clone();
+                            Some(tokio::task::spawn(async move {
+                                #callback(#args).await;
+                            }))
+                        }
+                    }
+                } else {
+                    let args = quote! { #(#field_names),* };
+                    quote! {
+                        #pattern => {
+                            Some(tokio::task::spawn(async move {
+                                #callback(#args).await;
+                            }))
+                        }
+                    }
+                }
+            } else {
+                quote! {
+                    #pattern => None
+                }
+            }
+        }
+        Fields::Named(fields) => {
+            let field_names: Vec<&syn::Ident> = fields
+                .named
+                .iter()
+                .filter_map(|f| f.ident.as_ref())
+                .collect();
+
+            if field_names.len() != fields.named.len() {
+                abort!(
+                    variant.ident.span(),
+                    "All named fields must have identifiers"
+                );
+            }
+
+            let pattern = quote! { Self::#variant_name { #(#field_names),* } };
+
+            if let Some(callback) = callback_name {
+                if has_sender {
+                    let args = quote! { tx_clone, #(#field_names),* };
+                    quote! {
+                        #pattern => {
+                            let tx_clone = tx.clone();
+                            Some(tokio::task::spawn(async move {
+                                #callback(#args).await;
+                            }))
+                        }
+                    }
+                } else {
+                    let args = quote! { #(#field_names),* };
+                    quote! {
+                        #pattern => {
+                            Some(tokio::task::spawn(async move {
+                                #callback(#args).await;
+                            }))
+                        }
+                    }
+                }
+            } else {
+                quote! {
+                    #pattern => None
+                }
+            }
+        }
+    }
+}
+
+fn extract_message_type(attrs: &[Attribute]) -> Option<syn::Path> {
+    for attr in attrs {
+        if attr.path().is_ident("taskspec") {
+            if let Meta::List(meta_list) = &attr.meta {
+                let nested = match NestedMeta::parse_meta_list(meta_list.tokens.clone()) {
+                    Ok(nested) => nested,
+                    Err(e) => abort!(attr.span(), "Invalid taskspec attribute syntax: {}", e),
+                };
+                let args = match EnumArgs::from_list(&nested) {
+                    Ok(args) => args,
+                    Err(e) => abort!(attr.span(), "Invalid taskspec attribute: {}", e),
+                };
+                return Some(args.message_type);
+            }
+        }
+    }
+    None
+}
+
+fn extract_callback_name(attrs: &[Attribute]) -> Option<syn::Ident> {
+    for attr in attrs {
+        if attr.path().is_ident("taskspec") {
+            if let Meta::List(meta_list) = &attr.meta {
+                let nested = match NestedMeta::parse_meta_list(meta_list.tokens.clone()) {
+                    Ok(nested) => nested,
+                    Err(e) => abort!(attr.span(), "Invalid taskspec attribute syntax: {}", e),
+                };
+                let args = match VariantArgs::from_list(&nested) {
+                    Ok(args) => args,
+                    Err(e) => abort!(attr.span(), "Invalid taskspec attribute: {}", e),
+                };
+                return Some(args.callback);
+            }
+        }
+    }
+    None
+}
+
+#[cfg(test)]
+mod tests {}
--- crates/azure-keyvault-tui/src/background.rs
+++ crates/azure-keyvault-tui/src/background.rs
@@ -1,18 +1,21 @@
-use azure_keyvault_tui_macros::{BackgroundTaskSpec, background_task};
+use async_spawn_macros::{TaskSpec, background_task};
 use tokio::sync::mpsc::{Receiver, Sender};
 
-use crate::{azure_api::get_access_token_for_subscription, tui::TuiEvent};
+use crate::{
+    azure_api::{get_access_token_for_subscription, list_key_vaults},
+    tui::TuiEvent,
+};
 
 /// Represents different types of background tasks that can be launched.
 ///
 /// Each [`TaskSpec`] contains the necessary parameters and other information to be able to call
 /// the function associated with the specified background task.
-#[derive(BackgroundTaskSpec)]
-#[taskspec(event_enum = "TuiEvent")]
-pub enum TaskSpec {
+#[derive(TaskSpec)]
+#[taskspec(message_type = "TuiEvent")]
+pub enum BackgroundTask {
     /// Lists Key Vaults for the given subscription ID.
-    #[taskspec(callback = "list_key_vaults")]
-    ListKeyVaults { subscription_id: String },
+    #[taskspec(callback = "load_key_vaults")]
+    LoadKeyVaults { subscription_id: String },
 }
 
 /// Launches requested tasks in the background and waits for tasks to finish before exiting.
@@ -20,15 +23,16 @@ pub enum TaskSpec {
 /// Continously launches new tokio tasks based on the [`TaskSpec`] receieved from the given
 /// [`Receiver`]. Passes a clone of the given [`Sender`] to the launched background tasks so the
 /// background tasks can send messages to the TUI thread to request state modifications.
-pub async fn manager(mut rx_bg_task: Receiver<TaskSpec>, tx_tui_event: Sender<TuiEvent>) {
+pub async fn manager(mut rx_bg_task: Receiver<BackgroundTask>, tx_tui_event: Sender<TuiEvent>) {
     let mut spawned_tasks = vec![];
     // Stay alive only while main thread is alive
     while let Some(task_spec) = rx_bg_task.recv().await {
         // Spawn a new task or thread for new BackgroundTasks and add them to the Vec to keep track
         // of them.
         // TODO: There might be a better way to keep track of them.
-        let handle = task_spec.spawn_task(&tx_tui_event);
-        spawned_tasks.push(handle);
+        if let Some(handle) = task_spec.spawn_task(&tx_tui_event) {
+            spawned_tasks.push(handle);
+        }
     }
 
     // Wait for all background tasks to finish.
@@ -41,10 +45,10 @@ pub async fn manager(mut rx_bg_task: Receiver<TaskSpec>, tx_tui_event: Sender<Tu
 }
 
 /// Lists Key Vaults for the given subscription and sends the result to the TUI.
-#[background_task(event_enum = "TuiEvent", error_variant = "TuiEvent::SetErrorStatus")]
-async fn list_key_vaults(subscription_id: String) {
+#[background_task(message_type = "TuiEvent", abort_with = "TuiEvent::SetErrorStatus")]
+async fn load_key_vaults(subscription_id: String) {
     // Show loading status
-    update_progress!(TuiEvent::SetSuccessStatus(
+    notify!(TuiEvent::SetSuccessStatus(
         "Loading Key Vaults...".to_string(),
     ));
 
@@ -57,12 +61,8 @@ async fn list_key_vaults(subscription_id: String) {
     };
 
     // List key vaults using the token
-    match crate::azure_api::list_key_vaults(&subscription_id, &access_token).await {
-        Ok(key_vaults) => {
-            update_progress!(TuiEvent::KeyVaultsLoaded(key_vaults));
-        }
-        Err(e) => {
-            abort!("Failed to load Key Vaults: {}", e);
-        }
+    match list_key_vaults(&subscription_id, &access_token).await {
+        Ok(key_vaults) => notify!(TuiEvent::KeyVaultsLoaded(key_vaults)),
+        Err(e) => abort!("Failed to load Key Vaults: {}", e),
     }
 }
